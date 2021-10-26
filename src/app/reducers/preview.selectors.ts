523964692c659c23b6aedf921ccac776bbfef9f1
Aliaksei <aliaksei.ihnatovich@leverx.com>
2021-10-26 22:54:28 +0300

    delete

--- src/app/reducers/preview.selectors.ts
+++
@@ -1,9 +0,0 @@
-import { createFeatureSelector, createSelector } from '@ngrx/store';
-import { PreviewState } from './preview';
-
-
-export namespace previewSelectors {
-  export const state = createFeatureSelector<PreviewState>('example');
-  export const previewData = createSelector(state, (state) => state.previewData);
-  export const selectedTags = createSelector(state, (state) => state.selectedTags)
-}
